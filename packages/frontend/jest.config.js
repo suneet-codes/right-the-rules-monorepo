72699cbd60e12ee40ce3d7347a36ca7bdd75271c
Suneet Misra <hello@suneet.codes>
2021-08-08 09:50:42 +0100

    Updated linting and formatting configs in each package and at root

--- packages/frontend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
-}
